c0cc102ff318cf995ef5c02e5dd08479d4dd78fc
Haardispro <haardmajmudar2827@gmail.com>
2024-11-15 09:02:33 +0530

    added show tasks button, database next in priority

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ w.resizable(height=False, width=False)
 #w.geometry("500x200")
 #Fonts
 #Heading font
-f1 = ('Cascadia Code', 25, 'bold')
-f2 = ('Cascadia Code', 15, 'bold')
+f1 = ('Agave', 25, 'bold')
+f2 = ('Agave', 15, 'bold')
+
 #Functions
-
-
-
 def add():
     w = enter.get()
     enter.delete(0, END)
@@ -28,6 +26,10 @@ def add():
 
 def remove():
     listbox.delete(ACTIVE)
+
+def show():
+    return  
+
 
 #Heading
 head = Label(w, text="Todo List", font=f1, bg="#1e1e1e", fg="white")
@@ -52,6 +54,7 @@ sb.config(command=listbox.yview)
 #Button and Entry
 remove = Button(w, text="Remove task", font=f1, command=remove)
 add = Button(w, text="Add task", font=f1, command=add)
+show = Button(w, text="Show Tasks", font=f1, command=show)
 enter_lab = Label(w, text="Enter a task:", font=f2, fg="white", bg="#1e1e1e")
 enter = Entry(w, width=25, fg="#1e1e1e", bg="white", font=f1)
 
@@ -63,6 +66,7 @@ enter_lab.grid(row=3, column=0, padx=20, pady=10)
 enter.grid(row=4, column=0, padx=20, pady=10)
 add.grid(row=5, column=0, padx=20, pady=10)
 remove.grid(row=6, column=0, padx=20, pady=10)
+show.grid(row=7, column=0, padx=20, pady=10)
 sb.grid(row=2, column=1, sticky="ns")
 
 w.mainloop()
